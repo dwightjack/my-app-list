4d678bdc128796c6c1a76f64ce8e7e34069e5f8b
dwightjack <marco.solazzi@gmail.com>
2023-03-27 18:44:57 +0900

    migrate to alpinejs

--- src/js/main.js
+++ src/js/main.js
@@ -1,66 +1,48 @@
-import { app, h, text } from 'https://cdn.skypack.dev/hyperapp@2';
+document.addEventListener('alpine:init', () => {
+  /**
+   * @typedef {'idle' | 'loaded' | 'loading' | ' error'} Status
+   */
 
-const initialState = {
-  status: 'idle',
-  repos: [],
-};
+  Alpine.store('status', 'idle');
 
-const SetStatus = (state, status) => ({ ...state, status });
+  /**
+   * @param status {Status}
+   */
+  const setStatus = (status) => {
+    Alpine.store('status', status);
+  };
 
-const GotProjects = (state, repos = []) => ({
-  ...SetStatus(state, 'loaded'),
-  repos,
-});
+  /**
+   * User type definition
+   * @typedef {Object} Repo
+   * @property {string} name
+   * @property {string} topics
+   * @property {string} homepageUrl
+   * @property {string} description
+   * @property {string} url
+   */
 
-function branch(prop, branches) {
-  return branches[prop] || branches.default;
-}
+  const repos = {
+    /**
+     * @type {Repo[]}
+     */
+    items: [],
+    async init() {
+      setStatus('loading');
+      this.items = [];
+      try {
+        const response = await fetch('/.netlify/functions/fetch-projects');
+        if (!response.ok) {
+          throw response;
+        }
+        this.items = response.json();
+        setStatus('loaded');
+      } catch (error) {
+        console.error(error);
+        setStatus('error');
+      }
+    },
+  };
 
-async function FetchProjects(dispatch) {
-  dispatch(SetStatus, 'loading');
-
-  try {
-    const response = await fetch('/.netlify/functions/fetch-projects');
-    if (!response.ok) {
-      throw response;
-    }
-    dispatch(GotProjects, await response.json());
-  } catch (error) {
-    console.error(error);
-    dispatch(SetStatus, 'error');
-  }
-}
-
-function Link(href, txt) {
-  return h('a', { target: '_blank', href }, text(txt));
-}
-
-function Label(txt) {
-  return h('span', { class: 'label' }, text(txt));
-}
-
-function RepoItem({ name, topics, homepageUrl, description, url }) {
-  return h('article', { class: 'card' }, [
-    h('header', {}, h('h2', {}, Link(homepageUrl, name))),
-    h('p', {}, [
-      h('div', {}, text(description)),
-      h('small', {}, Link(url, 'GitHub')),
-    ]),
-    h('p', { class: 'cluster' }, topics.map(Label)),
-  ]);
-}
-
-app({
-  init: [initialState, [FetchProjects]],
-  node: document.getElementById('main'),
-  view: ({ repos, status }) =>
-    h(
-      'main',
-      {},
-      branch(status, {
-        loading: h('p', {}, text('Loading...')),
-        loaded: h('div', {}, repos.map(RepoItem)),
-        error: h('p', {}, text('Error')),
-      }),
-    ),
+  Alpine.store('repos', repos);
 });
